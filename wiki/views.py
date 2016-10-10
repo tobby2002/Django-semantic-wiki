3540baf24a63f433dcb64aaa285d91a452b8343a
amoghaUdupa <amogha15@gmail.com>
2016-10-10 20:18:27 +0530

    faster link process with outlink check

--- wiki/views.py
+++ wiki/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
-from wiki.models import Page
+from wiki.models import Page, PageOutlinks
 from wikimarkup import parse
 import re
+
 LINK_FROMAT = """<a href="{path}">{linktext}</a>"""
+OUTLINK_TITLES = []
+
+_re_linktext = re.compile(r'\[\[([a-zA-Z0-9 ()-.]+)\]\]')
+_re_altlink = re.compile(r'\[\[([a-zA-Z0-9 ()-.]+)\|([a-zA-Z0-9 ()-.]+)\]\]')
+_re_media = re.compile(r'\[\[([a-zA-Z0-9 -,.:<>=""/_|]+)\]\]')
+_re_annotate = re.compile(r'\{\{([\'a-zA-Z0-9 -,.:<>=""/_|]+)\}\}')
 
 def processlink(m):
     if "|" in m.group(0):
@@ -12,8 +19,8 @@ def processlink(m):
         linktext = m.group(1)
     matchtext = m.group(1)
     matchtext = matchtext.replace(" ","_")
-    if not Page.objects.filter(name=matchtext).only("id").exists():
-        matchtext = u'Error_404'
+    if matchtext not in OUTLINK_TITLES:
+        return linktext
     path = "/wiki/" + matchtext
     return LINK_FROMAT.format(path = path,linktext = linktext)
 
@@ -21,10 +28,18 @@ def processmedia(m):
     """TODO: process the objects and fill up html to render media"""
     return ""
 
-def addlinks(text):
-    text = re.sub(r'\[\[([a-zA-Z0-9 ()-.]+)\]\]', processlink, text) # direct links
-    text = re.sub(r'\[\[([a-zA-Z0-9 ()-.]+)\|([a-zA-Z0-9 ()-.]+)\]\]', processlink, text) # links with
-    text = re.sub(r'\[\[.+?\]\] | \{\{.+?\}\}', "", text) # for File Wikitory and other wikimedia
+def getOutlinks(id):
+    outlinks = PageOutlinks.objects.filter(id=id)
+    for outLink in outlinks:
+        OUTLINK_TITLES.append(Page.objects.get(id=outLink.outlinks).name)
+
+def addlinks(id,text):
+    getOutlinks(id)
+    text = _re_linktext.sub(processlink, text) # direct links
+    text = _re_altlink.sub(processlink, text) # links with
+    text = _re_media.sub(processmedia, text) # for File Wikitory and other wikimedia
+    text = _re_annotate.sub(processmedia, text) # infobox and related info
+    OUTLINK_TITLES = []
     return text
 
 # Create your views here.
@@ -33,7 +48,7 @@ def article(request, page_name):
         page = Page.objects.get(name=page_name)
     except Page.DoesNotExist as e:
         raise Http404("No such page exists")
-    text = addlinks(parse(page.text))
+    text = addlinks(page.id, parse(page.text))
     context = {
     'name' : page.name,
     'text' : text,
